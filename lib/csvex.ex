a9d857f22e008ccf164778c1a591d0ab781e88e3
Martin Torhage <martin.torhage@gmail.com>
2017-08-16 23:31:18 +0200

    Fail trying to use streams

--- lib/csvex.ex
+++ lib/csvex.ex
@@ -12,7 +12,34 @@ defmodule Csvex do
       :world
 
   """
-  def hello do
-    :world
+  def parse_fold(csv_stream, acc, folder_maker, options \\ []) do
+    :csv.decode_fold(
+      {:maker, folder_maker},
+      acc,
+      stream_generator(csv_stream),
+      options)
   end
+
+  defp stream_generator(csv_stream) do
+    generator = fn (stream) ->
+      case Enum.take(stream, 1) do
+        [csv] -> {csv, Stream.drop(stream, 1)}
+        [] -> {"", :done}
+      end
+    end
+    {generator, csv_stream}
+  end
+
+  def run() do
+    csv_stream = File.stream!("/Users/kullervo/github/martin-torhage/csv/priv/benchtest.csv", [:raw])
+    folder_maker = fn(_) ->
+      folder = fn (_, acc) ->
+        acc + 1
+      end
+      capture = [1, 3, 2]
+      {folder, capture}
+    end
+    parse_fold(csv_stream, 0, folder_maker)
+  end
+
 end
